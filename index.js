8fe4e0da0d13a82d9ffe6d56e9846a706769e422
ryanfalco13@gmail.com <ryanfalco13@gmail.com>
2023-06-12 18:00:58 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Add your code here
+function submitData(name, email) {
+    const url = 'http://localhost:3000/users';
+    const headers = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    };
+    const data = {
+      name: name,
+      email: email
+    };
+    const body = JSON.stringify(data);
+  
+    return fetch(url, {
+      method: 'POST',
+      headers: headers,
+      body: body
+    })
+      .then(response => response.json())
+      .then(data => {
+        appendToDOM(data.id);
+      })
+      .catch(error => {
+        appendToDOM(error.message);
+      });
+  }
+  
+  function appendToDOM(value) {
+    const idDiv = document.createElement('div');
+    idDiv.textContent = value;
+    document.body.appendChild(idDiv);
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+ 
+  
